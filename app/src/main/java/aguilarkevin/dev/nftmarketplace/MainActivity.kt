8842f49cd186335940081a4a77c9607f55f954af
aguilarkevin <kevinaguilar0813@gmail.com>
2022-01-22 23:49:31 -0600

    add BottomNavigation

--- app/src/main/java/aguilarkevin/dev/nftmarketplace/MainActivity.kt
+++ app/src/main/java/aguilarkevin/dev/nftmarketplace/MainActivity.kt
@@ -3,36 +3,84 @@ package aguilarkevin.dev.nftmarketplace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import aguilarkevin.dev.nftmarketplace.ui.theme.NFTMarketplaceTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NFTMarketplaceTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Scaffold(
+                    bottomBar = {
+                        AppBottomNavigation()
+                    }
+                ) {
+                    // Screen content
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
+fun App(){
     NFTMarketplaceTheme {
-        Greeting("Android")
+        Scaffold(
+            bottomBar = {
+                AppBottomNavigation()
+            }
+        ) {
+            // Screen content
+
+        }
     }
 }
+
+@Composable
+fun AppBottomNavigation(){
+    val bottomNavigationItems = listOf(
+        R.drawable.ic_home,
+        R.drawable.ic_search,
+        R.drawable.ic_comment,
+        R.drawable.ic_user
+    )
+
+
+    BottomNavigation(
+        backgroundColor = Color(255,255,255),
+        modifier = Modifier.height(68.dp)
+    ) {
+        for (icon in bottomNavigationItems){
+            BottomNavigationItem(
+                selected = true,
+                onClick = { 1 + 1 },
+                modifier = Modifier.padding(
+                    PaddingValues(
+                        start = 0.dp,
+                        top = 20.dp,
+                        end = 0.dp,
+                        bottom = 20.dp
+                    )
+                ),
+                icon = {
+                    Icon(
+                        painterResource(id = icon),
+                        "home",
+                        Modifier.size(28.dp,28.dp),
+                        tint = Color(200,200,200)) },
+            )
+        }
+    }
+}
+
